94963c8d05c53aa866da843fa39ec1e28de5a59e
Dex0112 <dexcrazy06@gmail.com>
2024-06-17 14:27:43 -0400

    Feat: Simple input and display

--- c_gui/main.c
+++ c_gui/main.c
@@ -2,6 +2,7 @@
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_mouse.h>
+#include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_video.h>
@@ -10,6 +11,17 @@
 
 #define WINDOW_WIDTH 720
 #define WINDOW_HEIGHT 720
+
+SDL_Texture *load_texture(SDL_Renderer *renderer, char *path) {
+    SDL_Surface *loaded_surface = IMG_Load(path);
+
+    SDL_Texture *loaded_texture =
+        SDL_CreateTextureFromSurface(renderer, loaded_surface);
+
+    SDL_FreeSurface(loaded_surface);
+
+    return loaded_texture;
+}
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
@@ -22,21 +34,20 @@ int main() {
 
     IMG_Init(IMG_INIT_PNG);
 
-    SDL_Surface *board_surface = IMG_Load("./gfx/Board.png");
-    SDL_Surface *x_surface = IMG_Load("./gfx/Cross.png");
-    SDL_Surface *o_surface = IMG_Load("./gfx/Circle.png");
-
-    SDL_Texture *board_texture =
-        SDL_CreateTextureFromSurface(renderer, board_surface);
-    SDL_Texture *x_texture = SDL_CreateTextureFromSurface(renderer, x_surface);
-    SDL_Texture *o_texture = SDL_CreateTextureFromSurface(renderer, o_surface);
-
-    SDL_FreeSurface(board_surface);
-    SDL_FreeSurface(x_surface);
-    SDL_FreeSurface(o_surface);
+    SDL_Texture *board_texture = load_texture(renderer, "./gfx/Board.png");
+    SDL_Texture *x_texture = load_texture(renderer, "./gfx/Cross.png");
+    SDL_Texture *o_texture = load_texture(renderer, "./gfx/Circle.png");
 
     bool running = true;
     SDL_Event event;
+
+    int grid[3][3] = {
+        { -1, -1, -1 },
+        { -1, -1, -1 },
+        { -1, -1, -1 }
+    };
+
+    int turn_index = 0;
 
     while (running) {
         while (SDL_PollEvent(&event)) {
@@ -55,10 +66,12 @@ int main() {
 
                 SDL_GetMouseState(&x, &y);
 
-                printf("Click at (%d, %d)\n", x, y);
+                x /= WINDOW_WIDTH / 3;
+                y /= WINDOW_HEIGHT / 3;
 
-                printf("Grid Square: (%d, %d)\n", x / (WINDOW_WIDTH / 3),
-                       y / (WINDOW_HEIGHT / 3));
+                grid[x][y] = turn_index;
+
+                turn_index = 1 - turn_index;
             }
         }
 
@@ -67,20 +80,38 @@ int main() {
 
         SDL_Rect board_rect = {0, 0, WINDOW_WIDTH, WINDOW_HEIGHT};
 
-        SDL_Rect x_rect = {0, 0, WINDOW_WIDTH / 3, WINDOW_HEIGHT / 3};
-        SDL_Rect o_rect = {720 / 3, 720 / 3, WINDOW_WIDTH / 3, WINDOW_HEIGHT / 3};
-        SDL_Rect x_rect1 = {720 / 3 * 2, 720 / 3 * 2, WINDOW_WIDTH / 3, WINDOW_HEIGHT / 3};
 
         SDL_RenderCopy(renderer, board_texture, NULL, &board_rect);
-        SDL_RenderCopy(renderer, x_texture, NULL, &x_rect);
-        SDL_RenderCopy(renderer, x_texture, NULL, &o_rect);
-        SDL_RenderCopy(renderer, x_texture, NULL, &x_rect1);
+
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                if(grid[x][y] == -1) {
+                    continue;
+                }
+
+                int new_x = x * WINDOW_WIDTH / 3;
+                int new_y = y * WINDOW_HEIGHT / 3;
+
+                SDL_Rect token_rect = {
+                    new_x, new_y,
+                    WINDOW_WIDTH / 3, WINDOW_HEIGHT / 3,
+                };
+
+                SDL_Texture *texture = grid[x][y] == 0 ? x_texture : o_texture;
+
+                SDL_RenderCopy(renderer, texture, NULL, &token_rect);
+            }
+        }
 
         SDL_RenderPresent(renderer);
     }
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+
+    SDL_DestroyTexture(board_texture);
+    SDL_DestroyTexture(o_texture);
+    SDL_DestroyTexture(x_texture);
 
     return 0;
 }
